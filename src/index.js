3c67d557a09df4b33aad9ba7025e8ad67e5ce777
iamsanthoshh <santhoshjikumar1992@gmail.com>
2023-06-08 22:26:39 +0530

    Revert "new commit"
    This reverts commit 4538e5f96b3bcfb454f947138238779a76f99ae0.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'; import './App.css';
-class App extends Component { render() {
-return (
-<div className="App">
-        <h2>Welcome to React</h2>
-      </div>
-); }
-}
-export default App;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-sdadadadads
